551dc5b9bdb258ebf074377402ef9c2253108779
betterwebsite <normanwebdesigners@gmail.com>
2025-03-24 17:25:02 -0500

    First Update
    Jordan Babb

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,7 @@ const configServer = require("./src/config/server");
 // Other
 const filterPostDate = require("./src/config/postDate");
 const isProduction = configServer.isProduction;
+const eleventyPluginSharpImages = require("@codestitchofficial/eleventy-plugin-sharp-images");
 
 
 module.exports = function (eleventyConfig) {
@@ -36,6 +37,10 @@ module.exports = function (eleventyConfig) {
     /**=====================================================================
                                 END EXTENSIONS
     =======================================================================*/
+    eleventyConfig.addPlugin(eleventyPluginSharpImages, {
+        urlPath: "/assets/images",
+        outputDir: "public/assets/images",
+    });
 
 
     /**=====================================================================
