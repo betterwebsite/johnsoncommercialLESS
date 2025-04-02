04913f89bec8ba97b62b03d926ba11ecce5d0232
betterwebsite <normanwebdesigners@gmail.com>
2025-04-02 16:33:33 -0500

    tina updates
    Jordan Babb

--- .eleventy.js
+++ .eleventy.js
@@ -90,6 +90,8 @@ module.exports = function (eleventyConfig) {
     });
     eleventyConfig.addPassthroughCopy("./src/admin");
     eleventyConfig.addPassthroughCopy("./src/_redirects");
+    eleventyConfig.addPassthroughCopy("./admin"); // TinaCMS static admin UI
+
     /**=====================================================================
                               END PASSTHROUGHS
     =======================================================================*/
---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: 'C:/Users/jorda/OneDrive/Desktop/aaaWebsites/johnsoncommercialLESS/tina/__generated__/.cache/1743629401889', url: 'https://content.tinajs.io/1.5/content/70168c56-eee5-4c77-a613-0636ffccc0ad/github/main', token: 'b0e3a480ab22ab38ec5eb71a472903451aafb408', queries,  });
+export default client;
+  
